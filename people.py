20d65bae771964f407365d438ad5443c3882f573
Erin Polley <erinepolley@gmail.com>
2020-01-21 09:17:59 -0600

    successfully completed student exercise pt ii

---
+++ people.py
@@ -0,0 +1,6 @@
+class People:
+    def __init__(self):
+        self.first_name = ""
+        self.last_name = ""
+        self.slack = ""
+        self.cohort = ""
--- students.py
+++ students.py
@@ -1,11 +1,14 @@
 #first name, last name, slack handle, cohort, exercises[]
 
-class Student:
+from people import People 
+
+class Student(People):
     def __init__(self, first_name, last_name, slack, cohort):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.slack = slack
-        self.cohort = cohort
+        super().__init__()
+        # self.first_name = first_name
+        # self.last_name = last_name
+        # self.slack = slack
+        # self.cohort = cohort
         self.exercises = []
     def add_exercises(self, exercises_list):
         self.exercises.extend(exercises_list)
