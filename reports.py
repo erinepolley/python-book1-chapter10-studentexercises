f182990245c5aa10e5508639fbcfef7521b05938
Erin Polley <erinepolley@gmail.com>
2020-02-04 11:40:56 -0600

    completed another exercise in chapter

--- reports.py
+++ reports.py
@@ -1,12 +1,22 @@
 import sqlite3
 
+class Student():
+    def __init__(self, first_name, last_name, slack, cohort):
+        super().__init__()
+        self.first_name = first_name
+        self.last_name = last_name
+        self.slack = slack
+        self.cohort = cohort
+
+student = Student('Bart', 'Simpson', '@bart', 'Cohort 8')
+print(f'{student.first_name} {student.last_name} is in {student.cohort}')
 
 class StudentExerciseReports():
 
     """Methods for reports on the Student Exercises database"""
 
     def __init__(self):
-        self.db_path = "/absolute/path/to/home/directory/workspace/python/exercises/studentexercises/studentexercises.db"
+        self.db_path = "/Users/Erin Polley/workspace/python/book1/chapter10-studentexercises/studentexercises.db"
 
     def all_students(self):
 
@@ -17,14 +27,14 @@ class StudentExerciseReports():
 
             db_cursor.execute("""
             select s.Id,
-                s.FirstName,
-                s.LastName,
-                s.SlackHandle,
-                s.CohortId,
+                s.First_name,
+                s.Last_name,
+                s.Slack_handle,
+                s.Cohort_Id,
                 c.Name
             from Student s
-            join Cohort c on s.CohortId = c.Id
-            order by s.CohortId
+            join Cohort c on s.Cohort_Id = c.Id
+            order by s.Cohort_Id
             """)
 
             all_students = db_cursor.fetchall()
--- students.py
+++ students.py
@@ -5,10 +5,10 @@ from people import People
 class Student(People):
     def __init__(self, first_name, last_name, slack, cohort):
         super().__init__()
-        # self.first_name = first_name
-        # self.last_name = last_name
-        # self.slack = slack
-        # self.cohort = cohort
+        self.first_name = first_name
+        self.last_name = last_name
+        self.slack = slack
+        self.cohort = cohort
         self.exercises = []
     def add_exercises(self, exercises_list):
         self.exercises.extend(exercises_list)
